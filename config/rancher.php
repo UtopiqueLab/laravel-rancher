616640ba716dbe43b1f1f61c9770cfcf63a5ffe6
steevegldev <steevegl.dev@gmail.com>
2016-09-09 20:55:52 -0400

    Update rancher.php
    Magnified config keys

--- config/rancher.php
+++ config/rancher.php
@@ -6,7 +6,7 @@
 |
 */
 return [
-    'baseUrl'	=> env('RANCHER_BASE_URL') ? env('RANCHER_BASE_URL') : 'https://localhost:8080/v1/',
-    'accessKey'	=> env('RANCHER_ACCESS_KEY'),
-    'secretKey' => env('RANCHER_SECRET_KEY'),
+    'baseUrl'	=> env('RANCHER_API_ENDPOINT'),
+    'accessKey'	=> env('RACNHER_API_ACCESSKEY'),
+    'secretKey' => env('RANCHER_API_SECRETKEY'),
 ];
